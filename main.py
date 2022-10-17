c91ac698a788f9924c49053b59ade7e3b7469cd5
anatale <anatale@wastequip.com>
2022-10-16 20:33:37 -0400

    refactoring

--- main.py
+++ main.py
@@ -12,12 +12,6 @@
 # Also, the output contains parallel edges (-5).
 # Your implementation appears to run using the correct time complexity.
 
-
-import sys
-import fileinput
-import argparse
-
-
 class Stack:
     def __init__(self):
         self.items = []
@@ -39,101 +33,50 @@ class Stack:
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f')
-    args = parser.parse_args()
-    filename = args.f
-
     edges = []
     n = None
     e = None
+    print('Enter the graph representation. When you are finished, enter an empty line and the program will proces '
+          'your input')
+    while True:
+        try:
+            line = raw_input()
+            if line is None or line == "":
+                raise EOFError
+            if "\n" in line:
+                line = line.replace("\n", "")
+            # First input line is number of vertices
+            if n is None:
+                n = int(line)
+            # Second input line is number of edges
+            elif e is None:
+                e = int(line)
+            else:
+                stripped_line = line.strip()
+                edge_1 = int(stripped_line.split(" ")[0])
+                edge_2 = int(stripped_line.split(" ")[1])
+                edges.append([edge_1, edge_2])
+        except EOFError:
+            break
 
-    if filename is None:
-        print('Enter your graph representation and press Ctrl+D')
-        while True:
-            try:
-                line = raw_input()
-                if line is None or line == "":
-                    raise EOFError
-                if "\n" in line:
-                    line = line.replace("\n", "")
-                # First line is number of vertices
-                if n is None:
-                    n = int(line)
-                # Second line is number of edges
-                elif e is None:
-                    e = int(line)
-                else:
-                    stripped_line = line.strip()
-                    edge_1 = int(stripped_line.split(" ")[0])
-                    edge_2 = int(stripped_line.split(" ")[1])
-                    edges.append([edge_1, edge_2])
-            except EOFError:
-                break
-
-    else:
-        while True:
-            try:
-                inp = open(filename, "r")
-                line = inp.readline()
-                try:
-                    while line is not None and line != "":
-                        if "\n" in line:
-                            line = line.replace("\n", "")
-                        # First line is number of vertices
-                        if n is None:
-                            n = int(line)
-                        # Second line is number of edges
-                        elif e is None:
-                            e = int(line)
-                        else:
-                            stripped_line = line.strip()
-                            edge_1 = int(stripped_line.split(" ")[0])
-                            edge_2 = int(stripped_line.split(" ")[1])
-                            edges.append([edge_1, edge_2])
-                        line = inp.readline()
-                except EOFError:
-                    break
-                break
-            except IOError as e:
-                print("Please ensure that you are entering the file name properly")
-
-    print(n)
-    print(e)
-    print(edges)
-
-    # My test cases
-    # Test_1
-    # n = 8
-    # e = 9
-    # edges = [[0,1],[1,2],[2,3],[3,0],[2,4],[4,5],[5,6],[6,4],[6,7]]
-    # Test_2
-    # n = 8
-    # e = 11
-    # edges = [[0,1],[1,5],[0,2],[1,2],[3,1],[2,3],[3,6],[2,4],[3,4],[7,3],[4,7]]
-    # Test_3
-    # n = 5
-    # e = 5
-    # edges = [[1,0],[0,2],[2,1],[0,3],[1,4]]
-    # Test_4
-    # n = 6
-    # e = 6
-    # edges = [[0,1],[0,2],[1,3],[1,4],[4,5],[2,5]]
+    if e != len(edges):
+        print('You may be missing an edge or may have entered extra edges. Please check your input.')
 
     # Create graph representation and initialize visited list
-    adj_list = []
+    adjacency_list = [[]] * n
+    reverse_adjacency_list = [[]] * n
     visited = [False] * n
-    for a in range(n):
-        adj_list.append([])
+    reverse_visited = [False] * n
     for x in edges:
-        adj_list[x[0]].append(x[1])
+        adjacency_list[x[0]].append(x[1])
+        reverse_adjacency_list[x[1]].append(x[0])
 
     # Initialize the stack to record post order traversal
-    post_order = Stack()
-    post_order.push(0)
+    post_order_stack = Stack()
+    post_order_stack.push(0)
 
     # First DFS - Pushes vertices to the post_order stack as they (and their children) are done being explored
-    dfs_1(post_order.pop(), post_order, adj_list, visited)
+    dfs_build_post_order(post_order_stack.pop(), post_order_stack, adjacency_list, visited)
 
     # TODO: Change the following to work without explicitly constructing the reverse graph
     # Reverse the graph
@@ -141,12 +84,12 @@ def main():
         y = y.reverse()
 
     # Create new graph representation and reinitialize visited list to all False
-    adj_list = []
+    adjacency_list = []
     visited = [False] * n
     for a in range(n):
-        adj_list.append([])
+        adjacency_list.append([])
     for x in edges:
-        adj_list[x[0]].append(x[1])
+        adjacency_list[x[0]].append(x[1])
 
     # Initialize a list of SCCs
     scc = []
@@ -156,10 +99,10 @@ def main():
     # Second DFS - Uses the reversed graph to identify the strongly connected components
     # dfs_2(post_order.pop(), post_order, adj_list, visited, scc, curr_scc)
 
-    while post_order.has_elements() is True:
-        v = post_order.pop()
+    while post_order_stack.has_elements() is True:
+        v = post_order_stack.pop()
         if visited[v] is False:
-            dfs_2(v, post_order, adj_list, visited, scc, curr_scc)
+            dfs_2(v, post_order_stack, adjacency_list, visited, scc, curr_scc)
             scc.append(curr_scc)
             curr_scc = []
 
@@ -180,7 +123,7 @@ def main():
               "have connections to one another according to our adjacency list.")
 
 
-def dfs_1(v, post_order, adj_list, visited):
+def dfs_build_post_order(v, post_order, adj_list, visited):
     # Note that v has been visited
     visited[v] = True
     # For every adjacent node
@@ -188,7 +131,7 @@ def dfs_1(v, post_order, adj_list, visited):
         # If it hasn't been visited
         if visited[u] is False:
             # Call dfs_1 on it
-            dfs_1(u, post_order, adj_list, visited)
+            dfs_build_post_order(u, post_order, adj_list, visited)
     # If there is nowhere else to visit after v, add it to the processed nodes
     post_order.push(v)
 
